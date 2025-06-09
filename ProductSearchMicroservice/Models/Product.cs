d9cfb4634c8fe890b1322cf7041896710189752a
José Cerezo <joseenrique61@hotmail.com>
2025-06-09 18:32:28 -0500

    Created project and added mock method for GetAll() in ProductController

---
+++ ProductSearchMicroservice/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace ProductSearchMicroservice.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required double Price { get; set; }
+}
